b4069befc26620605baa46cdf15bcd44a65bbc80
Tomáš Sobota <67016313+sobotat@users.noreply.github.com>
2023-08-24 11:13:26 +0200

    Implemented basic messaging app ui
    Took 17 seconds

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:smart_file_sync/peerApi.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,115 +15,443 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'SFS',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue.shade200),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const SelectUsername(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class SelectUsername extends StatefulWidget {
+  const SelectUsername({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<SelectUsername> createState() => _SelectUsernameState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _SelectUsernameState extends State<SelectUsername> {
 
-  void _incrementCounter() {
+  String? username;
+  final controller = TextEditingController();
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return username == null ? Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+                Icons.wifi
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 10),
+              child: Text('SFS - Username'),
+            )
+          ],
+        ),
+      ),
+      body: Center(
+        child: FractionallySizedBox(
+          heightFactor: 0.2,
+          widthFactor: 0.9,
+          child: Container(
+            padding: const EdgeInsets.all(15),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.cyan.shade200,
+                  Colors.blueAccent.shade200,
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 10,
+                  child: TextField(
+                    controller: controller,
+                    decoration: InputDecoration(
+                      labelText: 'Username',
+                      errorText: (controller.text.isEmpty ? 'Fill Username' : null),
+                    ),
+                    textInputAction: TextInputAction.go,
+                    onSubmitted: (value) {
+                      setState(() {
+                        if (controller.text.isNotEmpty) {
+                          username = controller.text;
+                        }
+                      });
+                    },
+                    onChanged: (value) => setState(() { }),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          if (controller.text.isNotEmpty) {
+                            username = controller.text;
+                          }
+                        });
+                      },
+                      icon: const Icon(
+                        Icons.send,
+                      )
+                  ),
+                )
+              ],
+            )),
+          ),
+      ),
+    ) : MainPage(
+      username: username!,
+    );
+  }
+}
+
+
+class MainPage extends StatefulWidget {
+  const MainPage({
+    required this.username,
+    super.key,
+  });
+
+  final String username;
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+
+  PeerApi? peerApi;
+  List<String> messages = [];
+  String? description;
+  final connectStringController = TextEditingController();
+  final messageController = TextEditingController();
+  final messageFocus = FocusNode();
+  final scrollController = ScrollController();
+  int index = 0;
+  String connectionState = 'Closed';
+
+  @override
+  void initState() {
+    super.initState();
+    createPeer();
+  }
+
+  @override
+  void dispose() {
+    connectStringController.dispose();
+    messageController.dispose();
+    messageFocus.dispose();
+    scrollController.dispose();
+    if (peerApi != null) {
+      peerApi!.close();
+    }
+    super.dispose();
+  }
+
+  void createPeer() {
+    peerApi = PeerApi(userId: widget.username,
+      onIceDescription: (iceDescription) {
+        setState(() {
+          description = iceDescription;
+        });
+        Clipboard.setData(ClipboardData(text: description ?? ''));
+      },
+      onMessage: (message) {
+        setState(() {
+          messages.add(message);
+          scrollController.animateTo(messages.length * 45,
+            duration: const Duration(milliseconds: 100),
+            curve: Curves.ease,
+          );
+        });
+        debugPrint(' > :: $message');
+      },
+      onConnected: () { setState(() {
+        connectionState = 'Connected';
+      }); },
+      onConnecting: () { setState(() {
+        connectionState = 'Connecting';
+      }); },
+      onClosed: () { setState(() {
+        connectionState = 'Closed';
+      }); },
+      onDisconected: () { setState(() {
+        connectionState = 'Disconected';
+      }); },
+      onFailed: () { setState(() {
+        connectionState = 'Failed';
+      }); },
+    );
+    setState(() { });
+  }
+
+  void createOffer() {
+    if (peerApi == null) return;
+    peerApi!.createOffer();
+  }
+
+  void createAnswer() {
+    if (peerApi == null) return;
+    peerApi!.createAnswer();
+  }
+
+  void connect() {
+    if (peerApi == null) return;
+    peerApi!.connect(connectStringController.text);
+  }
+
+  void send() {
+    List<String> tmpData = [
+      'Hello',
+      'Test',
+      'Something',
+      'IDK',
+      'Peer',
+      'WTF',
+    ];
+
+    String message = messageController.text == '' ? tmpData[index] : messageController.text;
+
+    debugPrint('Trying send > $message');
+    peerApi!.sendMessage(message);
+
+    index += 1;
+    if (index >= tmpData.length) index = 0;
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      messageController.text = '';
+      messageFocus.requestFocus();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        title: const Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              Icons.wifi
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            Padding(
+              padding: EdgeInsets.only(left: 10),
+              child: Text('SFS - Chat'),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: Stack(
+        children: [
+          Center(
+            child: FractionallySizedBox(
+              heightFactor: 0.75,
+              widthFactor: 0.9,
+              child: Container(
+                padding: const EdgeInsets.all(15),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.cyan.shade200,
+                      Colors.blueAccent.shade200,
+                    ],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                  ),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.grey[100],
+                        ),
+                        child: ListView.builder(
+                          controller: scrollController,
+                          shrinkWrap: true,
+                          itemCount: messages.length,
+                          itemBuilder: (context, index) {
+                            Map<String,dynamic> message = jsonDecode(messages[index]);
+                            String user = message['username'] ?? '';
+
+                            return Container(
+                              width: 400,
+                              alignment: Alignment.centerLeft,
+                              margin: const EdgeInsets.all(5),
+                              padding: const EdgeInsets.all(5),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5),
+                                color: peerApi!.userId == user ? Colors.cyan[100] : Colors.grey[400],
+                              ),
+                              child: Row(
+                                mainAxisSize: MainAxisSize.max,
+                                children: [
+                                  Expanded(
+                                    flex: 5,
+                                    child: Align(
+                                      alignment: Alignment.centerLeft,
+                                      child: Row(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          Text(
+                                            user,
+                                            style: const TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                          SizedBox(width: 10,),
+                                          Flexible(
+                                            child: Text(
+                                              message['message'],
+                                              softWrap: true,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Align(
+                                      alignment: Alignment.centerRight,
+                                      child: Text(message['time']),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            );
+                          }
+                        ),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          flex: 10,
+                          child: TextField(
+                            controller: messageController,
+                            focusNode: messageFocus,
+                            decoration: const InputDecoration(
+                              labelText: 'Message Text',
+                            ),
+                            textInputAction: TextInputAction.go,
+                            onSubmitted: (value) {
+                              send();
+                            },
+                          ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: IconButton(
+                            onPressed: () {
+                              send();
+                            },
+                            icon: const Icon(
+                              Icons.send,
+                            )
+                          ),
+                        )
+                      ],
+                    ),
+                    connectionState != 'Connected' ? Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        TextField(
+                          controller: connectStringController,
+                          decoration: const InputDecoration(
+                            labelText: 'Connect Data',
+                          ),
+                          textInputAction: TextInputAction.go,
+                          onSubmitted: (value) {
+                            connect();
+                          },
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: ElevatedButton(
+                              onPressed: connect, child: const Text('Connect')),
+                        ),
+                        Text(connectionState,
+                          style: TextStyle(
+                            color: getConnectionStateColor(),
+                          ),
+                        ),
+                      ],
+                    ) : Container(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(2),
+            child: FloatingActionButton(
+              heroTag: 1,
+              onPressed: createOffer,
+              tooltip: 'Offer',
+              child: const Icon(Icons.arrow_forward),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(2),
+            child: FloatingActionButton(
+              heroTag: 2,
+              onPressed: createAnswer,
+              tooltip: 'Answer',
+              child: const Icon(Icons.arrow_back),
+            ),
+          ),
+        ],
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Color getConnectionStateColor() {
+    switch(connectionState) {
+      case 'Connected':
+        return Colors.lightGreen;
+      case 'Connecting':
+      return Colors.green;
+      case 'Closed':
+      return Colors.red;
+      case 'Failed':
+      return Colors.orangeAccent;
+      case 'Disconected':
+        return Colors.red.shade700;
+      default:
+        return Colors.purple;
+    }
   }
 }
